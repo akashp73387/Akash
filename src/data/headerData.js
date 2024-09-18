5acd9e15999c64851e8598ef1bed94ed38bbc12f
akash <akashp73387@gmail.com>
2024-09-18 11:09:21 +0530

    Completed

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -8,5 +8,5 @@ export const headerData = {
     "Hi, I'm Akash, a driven and creative MERN Full Stack Developer with a passion for building engaging and user-friendly web applications. My journey as a developer is rooted in a deep fascination for technology and a desire to use it to solve problems and create positive impact. I believe that the right technology, coupled with a thoughtful approach, can empower individuals and businesses in meaningful ways.",
   image: akash,
   resumePdf:
-    "https://drive.google.com/file/d/1NjgTL9-2ubSoYFIZFgx0G2jdaLRKpHZW/view",
+    "https://drive.google.com/file/d/1F_RjDL54F_hASvWgOLVGS2j22B5d4hmF/view",
 };
